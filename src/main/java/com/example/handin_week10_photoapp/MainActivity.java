5ca626a2286637b2da7c3b08b1995b021fe46947
Pedro Palma <pmcpalma@gmail.com>
2020-06-07 16:17:01 +0200

    Update MainActivity.java

--- src/main/java/com/example/handin_week10_photoapp/MainActivity.java
+++ src/main/java/com/example/handin_week10_photoapp/MainActivity.java
@@ -1,21 +1,26 @@
 package com.example.handin_week10_photoapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.handin_week10_photoapp.storage.FirebaseManager;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
@@ -25,39 +30,38 @@ import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    //find the image file
     private static final int PICK_IMAGE_REQUEST = 1;
-    //Buttons
     private Button select_btn;
     private Button upload_btn;
-    //ImageView
     private ImageView imageView;
-    // Uri object to store a file path
     public static Uri filePath;
 
-    //FirebaseStorage storage;
     StorageReference storageReference;
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         storageReference = FirebaseStorage.getInstance().getReference("images");
 
-        //getting views from the layout
         select_btn = findViewById(R.id.select_btn);
         upload_btn = findViewById(R.id.upload_btn);
 
         imageView = findViewById(R.id.imageView);
 
-        //attaching a listener
-        select_btn.setOnClickListener(this);
+        select_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
         upload_btn.setOnClickListener(this);
-
     }
 
     public void showImage() {
@@ -67,24 +71,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startActivityForResult(Intent.createChooser(intent, "select image"), PICK_IMAGE_REQUEST);
     }
 
-    //handle image chooser activity result
+
+    private void selectImage() {
+        final CharSequence[] options = {"Take Photo", "Choose from Gallery", "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Choose your profile picture");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Take Photo")) {
+                    Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePicture, 0);
+                } else if (options[item].equals("Choose from Gallery")) {
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickPhoto, 1);//one can be replaced with any action code
+                } else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            filePath = data.getData();
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
-                imageView.setImageBitmap(bitmap);
-
-            } catch (IOException e) {
-                e.printStackTrace();
+        if (resultCode != RESULT_CANCELED) {
+            switch (requestCode) {
+                case 0:  // take photo
+                    if (resultCode == RESULT_OK && data != null) {
+                        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                        imageView.setImageBitmap(bitmap);
+                        String path = MediaStore.Images.Media.insertImage(this.getContentResolver(), bitmap, "title", null);
+                        if (path != null) {
+                            filePath = Uri.parse(path);
+                        }
+                    }
+                    break;
+                case 1: // choose from gallery
+                    if (resultCode == RESULT_OK && data != null) {
+                        filePath = data.getData();
+                        try {
+                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                            imageView.setImageBitmap(bitmap);
+                        } catch (IOException e) {
+                        }
+                    }
+                    break;
             }
         }
     }
 
     //method to create file extension
-    private String getExtension(Uri uri) {
+    public String getExtension(Uri uri) {
         ContentResolver cr = getContentResolver();
         MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
         return mimeTypeMap.getExtensionFromMimeType(cr.getType(uri));
@@ -92,7 +131,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     // handle upload files
     public void uploadImage() {
-
         StorageReference ref = storageReference.child(System.currentTimeMillis() + "" + getExtension(filePath));
         ref.putFile(filePath)
                 .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
